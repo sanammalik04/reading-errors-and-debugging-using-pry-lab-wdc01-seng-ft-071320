cb2683e3d35e0191a7378801f9332afe04d24d47
Sanam Malik <sanam.malik04@gmail.com>
2020-06-23 20:17:03 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,11 @@
+require 'pry'
+
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
+  string 
   if string[0] == "s"
-    10 * "s" + string
+    string = ("s" *  10) + string
   else
     string
   end
--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,3 +1,4 @@
+
 # don't forget to add: require 'pry'
 
 def generate_star_date
@@ -10,6 +11,6 @@ def state_log(star_date)
 end
 
 def engage
-  puts state_log(date)
   date = generate_star_date
+  puts state_log(date)
 end
