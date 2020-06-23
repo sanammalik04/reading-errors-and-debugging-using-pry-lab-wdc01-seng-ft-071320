41ad8e08f390a82c919cbbb6d3f0a1f88b8feb5d
Sanam Malik <sanam.malik04@gmail.com>
2020-06-23 19:38:48 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 # don't forget to add: require 'pry'
 
-def snake_it_up("sssssssssssurprise!")
-  if string == "sssssssssssurprise!"
+def snake_it_up(string)
+  if string[0] == "s"
     10 * "s" + string
   else
     string
