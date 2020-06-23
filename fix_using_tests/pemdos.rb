b683651dd95b2bcfdd7bf25dd5e637e093645369
Sanam Malik <sanam.malik04@gmail.com>
2020-06-23 20:13:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,13 +3,10 @@ require 'pry'
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  binding.pry
   string 
   if string[0] == "s"
     string = 10 * "s" + string
-    binding.pry
   else
     string
-    binding.pry
   end
 end
